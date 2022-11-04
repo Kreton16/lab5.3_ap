82bb9249095836e7e31743dfa1b29fb7500fe970
Kreton16 <renko0708@gmail.com>
2022-11-04 05:16:10 +0200

    f_c

---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a = y(0);
+			Assert::AreEqual(a, 1.);
+		}
+	};
+}
